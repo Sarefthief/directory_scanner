aea0e71fbd2d26ee647d5acb85fafa46ae19467c
Sarefthief <Sarefthief@gmail.com>
2018-05-16 17:26:24 +0700

    added directory size calculation

--- src/com/company/DirectoryScanner.java
+++ src/com/company/DirectoryScanner.java
@@ -4,15 +4,18 @@ import org.apache.commons.io.FileUtils;
 
 public class DirectoryScanner
 {
-    public static void scan(String path)
+    public void scan(String path)
     {
+        long size = 0;
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
-        /*
-        long size = FileUtils.sizeOfDirectory(new File("E:/webserver"));
-        size /= 1024*1024;
-        System.out.println("Folder Size: " + size + " MB");
-        */
 
-    }
+        for (int i = 0; i < listOfFiles.length; i++){
+            if (listOfFiles[i].isDirectory()){
+                size = FileUtils.sizeOfDirectory(listOfFiles[i]);
+                size /= 1024*1024;
+                System.out.println("Folder Size: " + size + " MB");
+            }
+        }
+}
 }
