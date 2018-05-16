130dababf2da5b69195f209e2db67461ff1714af
Sarefthief <Sarefthief@gmail.com>
2018-05-16 13:28:34 +0700

    new branch, directory input

---
+++ src/com/company/DirectoryScanner.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class DirectoryScanner {
+
+    public static void main(String[] args) {
+
+        Scanner directory = new Scanner(System.in);
+        System.out.print("dir: ");
+        String name = directory.nextLine();
+        System.out.println(name);
+
+    }
+}
