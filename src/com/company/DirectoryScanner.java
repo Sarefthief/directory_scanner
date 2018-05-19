4d4870274df3049c9ce74dba9cb28c5bfdd578e2
Sarefthief <Sarefthief@gmail.com>
2018-05-19 14:21:22 +0700

    added parallel size calculation

--- src/com/company/DirectoryScanner.java
+++ src/com/company/DirectoryScanner.java
@@ -5,22 +5,13 @@ import org.apache.commons.io.FileUtils;
 
 public class DirectoryScanner
 {
-    public void scan(String path)
+    private void scan(String path)
     {
-        long size = 0;
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
 
         for (int i = 0; i < listOfFiles.length; i++){
-            if (listOfFiles[i].isDirectory()){
-                size = FileUtils.sizeOfDirectory(listOfFiles[i]);
-                size /= 1024*1024;
-                System.out.println("Name: " + listOfFiles[i].getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: true");
-            } else {
-                size = listOfFiles[i].length();
-                size /= 1024*1024;
-                System.out.println("Name: " + listOfFiles[i].getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: false");
-            }
+            new Thread(new Stream(listOfFiles[i])).start();
         }
     }
 
@@ -36,6 +27,29 @@ public class DirectoryScanner
             } catch (NullPointerException e) {
                 System.out.println("Wrong path. Enter a new one by following the example: \"C:\\path\\to\\your\\directory\" ");
             }
+        }
+    }
+}
+
+class Stream implements Runnable {
+
+    long size = 0;
+    File file;
+
+    Stream(File file){
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        if (file.isDirectory()){
+            size = FileUtils.sizeOfDirectory(file);
+            size /= 1024*1024;
+            System.out.println("Name: " + file.getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: true");
+        } else {
+            size = file.length();
+            size /= 1024*1024;
+            System.out.println("Name: " + file.getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: false");
         }
     }
 }
