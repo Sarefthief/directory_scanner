615edd34829f3a02950f995f055ae5f83ed3bcb4
Sarefthief <Sarefthief@gmail.com>
2018-05-21 16:10:58 +0700

    added threads join

--- src/com/company/DirectoryScanner.java
+++ src/com/company/DirectoryScanner.java
@@ -1,17 +1,26 @@
 package com.company;
 import java.io.File;
 import java.util.Scanner;
-import org.apache.commons.io.FileUtils;
+import java.lang.Thread;
+import java.util.WeakHashMap;
 
 public class DirectoryScanner
 {
     private void scan(String path)
     {
         File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
+        File[] listOfFiles  = folder.listFiles();
+        DirectoryThread[] myThreads = new DirectoryThread[listOfFiles.length];
 
         for (int i = 0; i < listOfFiles.length; i++){
-            new Thread(new Stream(listOfFiles[i])).start();
+            myThreads[i] = new DirectoryThread(listOfFiles[i]);
+        }
+        for (DirectoryThread thread:myThreads ){
+            try {
+                thread.t.join();
+            } catch (InterruptedException e) {
+                System.out.println("Поток прерван");
+            }
         }
     }
 
@@ -19,40 +28,15 @@ public class DirectoryScanner
     {
         DirectoryScanner dirScan = new DirectoryScanner();
         Scanner input = new Scanner(System.in);
-        while (true){
+
+        while(true){
             System.out.print("Full path to the directory: ");
             try {
                 String path = input.nextLine();
                 dirScan.scan(path);
-                break;
             } catch (NullPointerException e) {
                 System.out.println("Wrong path. Enter a new one by following the example: \"C:\\path\\to\\your\\directory\" ");
             }
-        }
-    }
-}
-
-class Stream implements Runnable
-{
-    long size = 0;
-    File file;
-
-    Stream(File file)
-    {
-        this.file = file;
-    }
-
-    @Override
-    public void run()
-    {
-        if (file.isDirectory()){
-            size = FileUtils.sizeOfDirectory(file);
-            size /= 1024*1024;
-            System.out.println("Name: " + file.getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: true");
-        } else {
-            size = file.length();
-            size /= 1024*1024;
-            System.out.println("Name: " + file.getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: false");
         }
     }
 }
---
+++ src/com/company/DirectoryThread.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+
+public class DirectoryThread implements Runnable
+{
+    Thread t;
+    long size = 0;
+    File file;
+
+    DirectoryThread(File file)
+    {
+        this.file = file;
+        t = new Thread ( this);
+        t.start() ;
+    }
+
+    @Override
+    public void run()
+    {
+        if (file.isDirectory()){
+            size = FileUtils.sizeOfDirectory(file);
+            size /= 1024*1024;
+            System.out.println("Name: " + file.getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: true");
+        } else {
+            size = file.length();
+            size /= 1024*1024;
+            System.out.println("Name: " + file.getName() + "\t\t Size: " + size + "MB" + "\t\t Directory: false");
+        }
+    }
+}
